5af91fd6da399160a6795cb7c6114ee0941ac191
mokhs00 <mokhs00@naver.com>
2021-06-07 19:03:31 +0900

    add : domain 추가
    domain 추가
    - Board
    - User
    - BoardType

---
+++ src/main/java/com/example/communitydemo/domain/User.java
@@ -0,0 +1,46 @@
+package com.example.communitydemo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @Builder
+    public User(Long id, String name, String password, String email,
+                LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+}
